6c5d2d50101c08b72ebcc45bf84d8746d665c33b
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-08-29 15:37:39 -0400

    Added the location class

---
+++ models/Location.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Location extends Model {}
+
+Location.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    location_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "location",
+  }
+);
+module.exports = Location;
