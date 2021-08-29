75db8c32088df96efc730f497683b29fd3a9beb9
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-08-29 15:01:13 -0400

    Added the traveler class

---
+++ models/Traveler.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Traveler extends Model {}
+
+Traveler.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "traveler",
+  }
+);
+module.exports = Traveler;
